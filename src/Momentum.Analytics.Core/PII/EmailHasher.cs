4a7d63b9a5720dadf77b57a9ce68ee58abaa5ef5
Kyle <k.c.bolay@gmail.com>
2024-05-15 07:40:07 -0500

    Improving email hashing
    Now allowing a to upper environment variable.
    Also making sure email is trimmed before hashing

--- src/Momentum.Analytics.Core/PII/EmailHasher.cs
+++ src/Momentum.Analytics.Core/PII/EmailHasher.cs
@@ -4,28 +4,45 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using Momentum.Analytics.Core.PII.Interfaces;
 
 namespace Momentum.Analytics.Core.PII
 {
     public class EmailHasher : IEmailHasher
     {
+        public const string EMAIL_TO_UPPER = "EMAIL_TO_UPPERCASE";
+        public const bool EMAIL_TO_UPPER_DEFAULT = false;
+        protected readonly bool _toUpperCase;
         protected readonly ILogger _logger;
 
-        public EmailHasher(ILogger<EmailHasher> logger)
+        public EmailHasher(IConfiguration configuration, ILogger<EmailHasher> logger)
         {
+            _toUpperCase = configuration.GetValue<bool>(EMAIL_TO_UPPER, EMAIL_TO_UPPER_DEFAULT);
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
+        } // end method
+
         public virtual async Task<string> HashEmailAsync(string email, CancellationToken token = default)
         {
             // Convert byte array to a string
             var result = new StringBuilder();
 
+            var emailToHash = email.Trim();
+            if(_toUpperCase)
+            {
+                emailToHash = emailToHash.ToUpperInvariant();
+            }
+            else
+            {
+                emailToHash = emailToHash.ToLowerInvariant();
+            } // end if
+
             // Create a SHA256
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(email));
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(emailToHash));
                 
                 for (int i = 0; i < bytes.Length; i++)
                 {
